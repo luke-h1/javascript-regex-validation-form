a3d6759810ff2847ff7f22d95dbc00ee4728f82f
luke-h1 <luke.howsam@yahoo.com>
2020-08-15 21:29:00 +0100

    fix name and postcode validation

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ document.getElementById('phone').addEventListener('blur', validatePhone);
 
 function ValidateName() {
     const name = document.getElementById('name');
-    const re = /^[a-zA-Z]{2, 10}$/;
+    const re = /^[a-zA-Z{2,10}]/; 
     if (!re.test(name.valie)) {
         name.classList.add('is-invalid');
     } else {
@@ -15,7 +15,15 @@ function ValidateName() {
     }
 }
 
-function validatePostcode() {} // TODO 
+function validatePostcode() {
+    const postCode = document.getElementById('postcode');
+    const re = /^[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}$/i;
+    if (!re.test(postcode.value)) {
+        postcode.classList.add('is-invalid');
+    } else {
+        postcode.classList.remove('is-invalid');
+    }
+}
 
 function validateEmail() {
     const email = document.getElementById('email');
@@ -29,7 +37,7 @@ function validateEmail() {
 
 function validatePhone() {
     const phone = document.getElementById('phone');
-    //const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/; // USA PHONE VALIDATION  
+    //const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/; // USA PHONE VALIDATION
     const re = /^\+[44]\d[0-9]{11}/; //UK VALIDATION (MUST INCLUDE +44 AT START )
     if (!re.test(phone.value)) {
         phone.classList.add('is-invalid');
@@ -38,8 +46,4 @@ function validatePhone() {
     }
 }
 
-
-// six or seven for uk postcode 
-
-
-
+// six or seven for uk postcode
