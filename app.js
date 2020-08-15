2b024f0aaa982c2f9d3ead72fe45786d78493f92
luke-h1 <luke.howsam@yahoo.com>
2020-08-15 21:34:33 +0100

    fix typos in regex

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ document.getElementById('postcode').addEventListener('blur', validatePostcode);
 document.getElementById('email').addEventListener('blur', validateEmail);
 document.getElementById('phone').addEventListener('blur', validatePhone);
 
-function ValidateName() {
+
+function validateName() {
     const name = document.getElementById('name');
-    const re = /^[a-zA-Z{2,10}]/; 
-    if (!re.test(name.valie)) {
+    const re = /^[a-zA-Z]{2, 10}$/;
+    if (!re.test(name.value)) {
         name.classList.add('is-invalid');
     } else {
         name.classList.remove('is-invalid');
     }
 }
-
 function validatePostcode() {
-    const postCode = document.getElementById('postcode');
+    const postcode = document.getElementById('postcode');
     const re = /^[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}$/i;
     if (!re.test(postcode.value)) {
         postcode.classList.add('is-invalid');
@@ -31,14 +31,14 @@ function validateEmail() {
     if (!re.test(email.value)) {
         email.classList.add('is-invalid');
     } else {
-        email.classList, remove('is-invalid');
+        email.classList.remove('is-invalid');
     }
 }
 
 function validatePhone() {
     const phone = document.getElementById('phone');
     //const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/; // USA PHONE VALIDATION
-    const re = /^\+[44]\d[0-9]{11}/; //UK VALIDATION (MUST INCLUDE +44 AT START )
+    const re = /^\+[44]\d[0-9]{11}$/; //UK VALIDATION (MUST INCLUDE +44 AT START )
     if (!re.test(phone.value)) {
         phone.classList.add('is-invalid');
     } else {
